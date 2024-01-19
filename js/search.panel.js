15bc7b809b687c77b38848ac2585623bd641a639
Iryna1023 <148445292+Iryna1023@users.noreply.github.com>
2024-01-19 21:47:52 +0200

    Update search.panel.js

--- js/search.panel.js
+++ js/search.panel.js
@@ -1,15 +1,1 @@
-document.getElementById("searchButton").addEventListener("click", function () {
-    searchProducts();
-});
-function searchProducts() {
-    let searchQuery = document.getElementById("searchInput").value.toLowerCase();
-    allProducts = document.querySelectorAll(".item__box");
-    allProducts.forEach(function (product) {
-        let productTitle = product.querySelector(".item__title p").textContent.toLowerCase();  
-        if (productTitle.includes(searchQuery)) {      
-            product.style.display = "block";
-        } else {
-            product.style.display = "none";
-        }
-    });
-}
+
