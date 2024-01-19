b6b7dc643016404e6e4b09aafa742ec00577f717
Iryna1023 <148445292+Iryna1023@users.noreply.github.com>
2024-01-19 21:51:01 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("partialsLoaded", () => {
-    import("./modal.js");
     import("./header.js");
     import("./search.panel.js"); 
+    import("./cart.js");
+    import("./admin.js");
+    import("./logic.js");
+    import("./laptop.js");
+    import("./education-tree.js");
+
     
-  });
+  }); 
